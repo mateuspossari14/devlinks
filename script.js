805ae9b0b42aac68e5f1baae8fe018126dac5040
Possari <126827804+mateuspossari14@users.noreply.github.com>
2023-07-08 13:57:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto de Mayk Brito, usando óculos e jaqueta preta, sem barba e fundo azul")
   } else {
     // se tiver sem light mode, manter a imagem normal
     img.setAttribute("src", "./assets/avatar.png")
